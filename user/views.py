65cf55c761542b5d01b13c7c5a37c1c38b5f7b80
Alexey Pechorin <Alexey.p@taboola.com>
2019-06-14 17:33:52 +0300

    Initial commit

---
+++ user/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+# Create your views here.
+from picture_interface.search import getImgSrc
+
+class IndexView(View):
+    def get(self, request):
+        s = request.GET.get('s', "")
+        img_list=[]
+        msg=''
+        if s:
+            try:
+                img_list=getImgSrc(s)
+            except Exception as e:
+                msg=str(e)+'NOT EXISTING'
+        return render(request, "index.html", {
+            "search":s,
+            "img_list": img_list,
+            "msg":msg,
+        })
+
+
+class InfoView(View):
+    def get(self, request):
+        return render(request, "info.html", {
+        })
+
+    def post(self,request):
+        img = request.POST.get("img", "")
+        return render(request, "info.html", {
+            "img": img,
+        })
